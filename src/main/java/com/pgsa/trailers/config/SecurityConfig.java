2914b8d166a617b68190169eff7d4477378320cf
Zelesto <theozwane@gmail.com>
2026-06-04 11:51:15 +0200

    Add HttpMethod import to SecurityConfig 1151

--- src/main/java/com/pgsa/trailers/config/SecurityConfig.java
+++ src/main/java/com/pgsa/trailers/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.http.HttpMethod;
 
 import java.util.List;
 
