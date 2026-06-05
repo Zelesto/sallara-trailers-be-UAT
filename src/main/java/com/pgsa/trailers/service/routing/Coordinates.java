1d8ee3bf2d56b25f6b77c6a2f616b55ad65b6489
Zelesto <theozwane@gmail.com>
2026-06-05 13:09:42 +0200

    Add Coordinates class with latitude and longitude

--- src/main/java/com/pgsa/trailers/service/routing/Coordinates.java
+++ src/main/java/com/pgsa/trailers/service/routing/Coordinates.java
@@ -1,1 +1,15 @@
+package com.pgsa.trailers.service.routing;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Coordinates {
+    private double lat;
+    private double lng;
+
+    public String toQuery() {
+        return lng + "," + lat;
+    }
+}
