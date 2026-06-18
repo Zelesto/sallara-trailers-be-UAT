f65bfcd0eece80c6e2d518dd720adbff6b4e4af8
Zelesto <theozwane@gmail.com>
2026-06-18 15:03:29 +0200

    Remove TripMetricsCalculationListener class 1503
    Remove TripMetricsCalculationListener as its functionality is now handled by TripMetricsEventListener.

--- src/main/java/com/pgsa/trailers/service/listener/TripMetricsCalculationListener.java
+++ src/main/java/com/pgsa/trailers/service/listener/TripMetricsCalculationListener.java
@@ -1,22 +1,2 @@
 package com.pgsa.trailers.service.listener;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class TripMetricsCalculationListener {
-
-    @Async
-    @EventListener
-    public void handle(TripMetricsCalculationEvent event) {
-        // This is now handled by TripMetricsEventListener.onTripPlanned()
-        log.debug("TripMetricsCalculationEvent received for trip {} - metrics will be calculated by TripMetricsEventListener", 
-            event.tripId());
-        // Do nothing - let TripMetricsEventListener handle it
-    }
-}
