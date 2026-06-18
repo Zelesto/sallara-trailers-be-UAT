b7955d579d5fd754ab043a03aaa62dc4b11f0821
Zelesto <theozwane@gmail.com>
2026-06-18 10:59:14 +0200

    Add RouteCalculationRequestDTO import to TripService 1059

--- src/main/java/com/pgsa/trailers/service/TripService.java
+++ src/main/java/com/pgsa/trailers/service/TripService.java
@@ -25,6 +25,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.pgsa.trailers.dto.RouteCalculationRequestDTO;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
