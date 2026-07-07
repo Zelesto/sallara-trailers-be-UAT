e1e88d9bb3a33b3a7ad886f2e53b9604aef19f58
Zelesto <theozwane@gmail.com>
2026-07-07 13:59:20 +0200

    Add HashSet and Set imports to DriverService 1359

--- src/main/java/com/pgsa/trailers/service/DriverService.java
+++ src/main/java/com/pgsa/trailers/service/DriverService.java
@@ -18,6 +18,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.HashSet; 
+import java.util.Set;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
