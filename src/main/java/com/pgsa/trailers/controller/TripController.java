def61e75e57aa3d2a653af60fa0c62d6279266d9
Zelesto <theozwane@gmail.com>
2026-07-08 09:53:34 +0200

    Refactor imports in TripController.java 0953

--- src/main/java/com/pgsa/trailers/controller/TripController.java
+++ src/main/java/com/pgsa/trailers/controller/TripController.java
@@ -23,9 +23,9 @@ import com.pgsa.trailers.dto.UpdateTripRequest;
 import jakarta.validation.Valid;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.domain.Sort;
 
 @RestController
