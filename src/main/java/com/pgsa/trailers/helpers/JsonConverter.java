ae126049e252323d8f7783e6271d089fcbc5b1c1
Zelesto <theozwane@gmail.com>
2026-07-09 15:07:38 +0200

    Comment out PGobject import in JsonConverter 1507
    Comment out the import statement for PGobject.

--- src/main/java/com/pgsa/trailers/helpers/JsonConverter.java
+++ src/main/java/com/pgsa/trailers/helpers/JsonConverter.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import org.postgresql.util.PGobject;
+//import org.postgresql.util.PGobject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
