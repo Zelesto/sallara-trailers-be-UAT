e2125052c4c565b04ea51f5a5a22783d32bc764e
Zelesto <theozwane@gmail.com>
2026-07-09 11:20:01 +0200

    Add PodStatus enum for trailer statuses 1119

---
+++ src/main/java/com/pgsa/trailers/enums/PodStatus.java
@@ -0,0 +1,11 @@
+// src/main/java/com/pgsa/trailers/enums/PodStatus.java
+package com.pgsa.trailers.enums;
+
+public enum PodStatus {
+    SCANNED,
+    PENDING,
+    DELIVERED,
+    VERIFIED,
+    REJECTED,
+    CANCELLED
+}
