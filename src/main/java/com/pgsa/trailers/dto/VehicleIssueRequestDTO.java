ed1bce555014957ac47e1147e2f42be703bbc20f
Zelesto <theozwane@gmail.com>
2026-07-28 13:44:59 +0200

    Create VehicleIssueRequestDTO class for vehicle issues 1344

---
+++ src/main/java/com/pgsa/trailers/dto/VehicleIssueRequestDTO.java
@@ -0,0 +1,24 @@
+// src/main/java/com/pgsa/trailers/dto/VehicleIssueRequestDTO.java
+package com.pgsa.trailers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleIssueRequestDTO {
+    private Long vehicleId;
+    private Long driverId;
+    private Long tripId;
+    private LocalDateTime issueDate;
+    private String notes;
+    private List<VehicleIssueItemRequestDTO> items;
+}
+
