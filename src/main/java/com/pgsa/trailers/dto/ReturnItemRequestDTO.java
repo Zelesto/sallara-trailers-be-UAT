ff2b349b88b6cd1a943ea2107c1b74cdb4a1c4d6
Zelesto <theozwane@gmail.com>
2026-07-28 13:48:11 +0200

    Add ReturnItemRequestDTO class for return item requests 1348

---
+++ src/main/java/com/pgsa/trailers/dto/ReturnItemRequestDTO.java
@@ -0,0 +1,20 @@
+// src/main/java/com/pgsa/trailers/dto/ReturnItemRequestDTO.java
+package com.pgsa.trailers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReturnItemRequestDTO {
+    private Long itemId;
+    private BigDecimal quantity;
+    private String condition;
+    private String notes;
+}
