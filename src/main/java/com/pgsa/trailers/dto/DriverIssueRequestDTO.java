092607e3d70b37cb99ccc9193e2ec5169592705f
Zelesto <theozwane@gmail.com>
2026-07-29 08:41:41 +0200

    Create DriverIssueRequestDTO class with fields 0841

---
+++ src/main/java/com/pgsa/trailers/dto/DriverIssueRequestDTO.java
@@ -0,0 +1,20 @@
+package com.pgsa.trailers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverIssueRequestDTO {
+    private Long driverId;
+    private LocalDateTime issueDate;
+    private String notes;
+    private List<DriverIssueItemRequestDTO> items;
+}
