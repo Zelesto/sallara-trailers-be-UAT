bee7194ac799ed151a6277cf62284538c1080c5c
Zelesto <theozwane@gmail.com>
2026-07-29 08:44:47 +0200

    Create DriverIssueItemResponseDTO class 0844

---
+++ src/main/java/com/pgsa/trailers/dto/DriverIssueItemResponseDTO.java
@@ -0,0 +1,25 @@
+package com.pgsa.trailers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverIssueItemResponseDTO {
+    private Long id;
+    private Long itemId;
+    private String itemName;
+    private String itemCategory;
+    private BigDecimal quantityIssued;
+    private BigDecimal quantityReturned;
+    private BigDecimal quantityOutstanding;
+    private String conditionIssued;
+    private String conditionReturned;
+    private String notes;
+}
