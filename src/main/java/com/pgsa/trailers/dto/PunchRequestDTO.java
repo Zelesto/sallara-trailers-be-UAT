82fe89bd6b088f49b07b16559fdef414d4a7e73b
Zelesto <theozwane@gmail.com>
2026-08-04 14:20:54 +0200

    Enhance PunchRequestDTO with location details
    Added new fields for location, latitude, and longitude to PunchRequestDTO.

--- src/main/java/com/pgsa/trailers/dto/PunchRequestDTO.java
+++ src/main/java/com/pgsa/trailers/dto/PunchRequestDTO.java
@@ -1,20 +1,28 @@
 // src/main/java/com/pgsa/trailers/dto/PunchRequestDTO.java
 package com.pgsa.trailers.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PunchRequestDTO {
-    @JsonProperty("driver_id")  // Map from snake_case
+    
+    @JsonProperty("driver_id")
     private Long driverId;
     
     @JsonProperty("punch_type")
     private String punchType;
     
+    @JsonProperty("location")
     private String location;
+    
+    @JsonProperty("latitude")
     private BigDecimal latitude;
+    
+    @JsonProperty("longitude")
     private BigDecimal longitude;
 }
-
