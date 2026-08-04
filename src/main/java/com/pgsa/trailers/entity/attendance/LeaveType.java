e3afa34553bd5a52569d1591f35685d04f0f0486
Zelesto <theozwane@gmail.com>
2026-08-04 11:27:39 +0200

    Import LocalDateTime in LeaveType.java 1127
    Added import for LocalDateTime to LeaveType.java

--- src/main/java/com/pgsa/trailers/entity/attendance/LeaveType.java
+++ src/main/java/com/pgsa/trailers/entity/attendance/LeaveType.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 
 @Entity
