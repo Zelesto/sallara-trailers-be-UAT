781ba9eb36d6033b6f0e345c2e87d8bfe81aca98
Zelesto <theozwane@gmail.com>
2026-08-06 10:53:31 +0200

    Add description field to CertificateRequest 1053
    Added a description field to CertificateRequest class.

--- src/main/java/com/pgsa/trailers/dto/CertificateRequest.java
+++ src/main/java/com/pgsa/trailers/dto/CertificateRequest.java
@@ -1,23 +1,17 @@
-// Create CertificateRequest.java
+// src/main/java/com/pgsa/trailers/dto/CertificateRequest.java
 package com.pgsa.trailers.dto;
 
 import lombok.Data;
-import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDate;
 
 @Data
 public class CertificateRequest {
-    @NotBlank(message = "Certificate type is required")
     private String type;
-    
     private String number;
-    
     private LocalDate issueDate;
-    
     private LocalDate expiryDate;
-    
     private String issuer;
-    
     private String documentUrl;
+    private String description;
 }
-
